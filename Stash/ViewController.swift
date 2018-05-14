6c05ddb30883bbbbe6110c50977bc728bcc757af
Gino Wu <gino.wu@viewthespace.com>
2018-05-14 18:50:09 -0400

    Initialized project

---
+++ Stash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stash
+//
+//  Created by Gino Wu on 5/14/18.
+//  Copyright © 2018 Gino Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
